0cca9bc3d2b954f592e621ae36771038173b12f8
mokurin000 <1348292515a@gmail.com>
2025-04-11 00:58:13 +0800

    chore: also output CERNET, founderbn

--- ip_to_merged_cidr_list.py
+++ ip_to_merged_cidr_list.py
@@ -9,15 +9,27 @@ def merge_cidr_list(input_csv: str, output_csv: str):
 
     df = pl.read_csv(mid_file)
     df = df.select([pl.col("CIDR列表", "运营商")])
-    df = df.with_columns(pl.col("运营商").str.replace_all(".*广电$", "广电"))
+    df = df.with_columns(
+        pl.col("运营商")
+        .str.replace_all(".*广电$", "广电")
+        .str.replace_all(".*大学$", "教育网")
+    )
 
     df = df.group_by(pl.col("运营商")).agg(pl.col("CIDR列表").str.join(", "))
     df.write_csv(output_csv)
 
     output_csv_selected = output_csv.replace(".csv", "-isp.csv")
-    df.filter(pl.col("运营商").is_in(["电信", "广电", "移动", "联通"])).write_csv(
-        output_csv_selected
-    )
+    ISP_IN_CHINA = [
+        "教育网",
+        "电信",
+        "联通",
+        "移动",
+        "广电",
+        "方正宽带",
+    ]
+    df.filter(pl.col("运营商").is_in(ISP_IN_CHINA)).sort(
+        by=pl.col("运营商").map_elements(ISP_IN_CHINA.index, return_dtype=pl.UInt32)
+    ).write_csv(output_csv_selected)
 
 
 if __name__ == "__main__":
